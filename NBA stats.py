a7e8a442e475428571160c81202b9f1b17b182f0
Fadi7anna <97726330+Fadi7anna@users.noreply.github.com>
2022-10-15 14:01:48 +0200

    Add files via upload

---
+++ NBA stats.py
@@ -0,0 +1,54 @@
+from urllib import response
+from requests import get
+from pprint import PrettyPrinter, pp
+
+BASE_URL = "http://data.nba.net/"
+ALL_JSON = "/prod/v1/today.json"
+
+printer = PrettyPrinter()
+
+data = get(BASE_URL + ALL_JSON).json()
+
+
+def get_links():
+    data = get(BASE_URL + ALL_JSON).json()
+    links = data['links']
+    return links
+
+
+def get_scoreboard():
+    scoreboard = get_links()['currentScoreboard']
+    games = get(BASE_URL + scoreboard).json()['games']
+
+    for game in games:
+        home_team = game['hTeam']
+        away_team = game['vTeam']
+        clock = game['clock']
+        period = game['period']
+        
+
+        print("---------------------------------------")
+        # print(f"{home_team} vs {away_team}, {clock}, {period}")
+        print(f"{home_team['triCode']} vs {away_team['triCode']}, {clock}, {period}")
+        print(f"{home_team['score']} - {away_team['score']}")
+        print(f"{clock} - {period['current']}")
+
+def get_stats():
+    stats = get_links()['leagueTeamStatsLeaders']
+    teams = get(BASE_URL + stats).json()['league']['standard']['regularSeason']['teams']
+
+    teams = list(filter(lambda x: x['name'] != "Team", teams))               #FILTERING FUNCTION
+    # teams.sort(key = lambda x: int(x['ppg']['rank']))                        #Sorting according to rank for every team
+
+
+    for i, team in enumerate(teams):
+        name = team['name']
+        nickname = team['nickname']
+        ppg = team['ppg']['avg']
+
+        print(f"{i + 1}. {name} - {nickname} - {ppg}")
+
+    # printer.pprint(teams[0].keys())
+
+get_stats()
+
